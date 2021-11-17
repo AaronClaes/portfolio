7b07beb2595195762750f8417d180d12c97b3244
Aaron Claes <aaronclaes22@gmail.com>
2021-11-17 21:07:37 +0100

    feat: add open graph meta tags

--- src/components/ReactHelmet.js
+++ src/components/ReactHelmet.js
@@ -4,10 +4,17 @@ import { Helmet } from "react-helmet";
 function ReactHelmet({
   title,
   description = "I am a Full Stack web developer on a journey in the digital world!",
+  type = "website",
+  image = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png",
+  path = "",
 }) {
   return (
     <div>
       <Helmet>
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content={type} />
+        <meta property="og:image" content={image} />
+        <meta property="og:url" content={`https://aaronclaes.be/${path}`} />
         <title>{title}</title>
         <meta name="title" content={title} />
         <meta name="description" content={description} />
